e448496ceb91f042d6e561928198dcd1ae8e2420
github username <github email address>
2019-04-19 10:35:13 -0700

    Final

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :score)
+  end
+
+end
